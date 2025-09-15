84a238a35ee0e29c26e7ecb0ef54e6a4ed1a381c
Jonathan R. Warden <john.warden@gmail.com>
2025-09-15 18:35:48 -0400

    Fix the well-known endpoint

--- packages/notes/src/well-known.ts
+++ packages/notes/src/well-known.ts
@@ -6,10 +6,11 @@ export default function wellKnown(app: express.Application, ctx: AppContext) {
   // getFeedSkeleton endpoint - use direct Express route
   app.get(
     '/.well-known/did.json',
-    asyncHandler(async (_req, res) => {
-      // if (!ctx.repoAccount.did.endsWith(ctx.feedgenDocumentDid)) {
-      //   return res.sendStatus(404)
-      // }
+    asyncHandler(async (req, res) => {
+      const hostname = req.get('host') || 'localhost'
+      if (!ctx.feedgenDocumentDid.endsWith(hostname)) {
+        return res.sendStatus(404)
+      }
       res.json({
         '@context': ['https://www.w3.org/ns/did/v1'],
         id: ctx.feedgenDocumentDid,
@@ -17,8 +18,8 @@ export default function wellKnown(app: express.Application, ctx: AppContext) {
           {
             id: '#bsky_fg',
             type: 'BskyFeedGenerator',
-            serviceEndpoint: `http://localhost:${ctx.config.port}`,
-            // serviceEndpoint: `https://${ctx.hostname}`,
+            // serviceEndpoint: `http://localhost:${ctx.config.port}`,
+            serviceEndpoint: `https://${hostname}`,
           },
         ],
       })
